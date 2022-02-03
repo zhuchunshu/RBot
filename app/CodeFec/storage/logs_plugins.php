cfc959d0e65c8688ca5f0743c93f9f95495839f9
zhuchunshu <laravel@88.com>
2022-02-03 22:46:14 +0800

    更新

--- app/CodeFec/storage/logs_plugins.php
+++ app/CodeFec/storage/logs_plugins.php
@@ -1,2 +1,2 @@
-- 2022-01-21 16:11:52插件状态变动:
-["Core","Mail","User","Topic","Comment","Search","XueXi","Test","Merge"]
+- 2022-01-31 12:15:07插件状态变动:
+["Core","Mail","User","Topic","Comment","Search","XueXi","Test","\u9752\u9f99"]
